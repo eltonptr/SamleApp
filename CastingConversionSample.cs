cd2ac6b769801790d5e2c3c3841d5691dda46d87
Elton Peter <Elton.Peter@Assurant.com>
2021-10-25 15:45:07 +0530

    Intial commit

---
+++ CastingConversionSample.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp
+{
+    // Create a console app to show basic calculations
+    // Addtion, Subtraction, Multiplication and Division
+    // Proper Exceptions needs to be handled and print generic message in console
+
+    // Classroom Session Assignment - Day1 
+    public class CastingConversionSample
+    {
+
+    }
+
+    class Animal
+    {
+        public void Eat() { Console.WriteLine("Eating."); }
+        public override string ToString()
+        {
+            return "I am an animal.";
+        }
+    }
+    class Mammal : Animal { }
+    class Giraffe : Mammal { }
+
+    class SuperNova { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var g = new Giraffe();
+            var a = new Animal();
+            //FeedMammals(g);
+            //FeedMammals(a);
+            // Output:
+            // Eating.
+            // Animal is not a Mammal
+
+            SuperNova sn = new SuperNova();
+            TestForMammals(g);
+            TestForMammals(sn);
+            // Output:
+            // I am an animal.
+            // SuperNova is not a Mammal
+        }
+
+        static void FeedMammals(Animal a)
+        {
+            if (a is Mammal m)
+            {
+                m.Eat();
+            }
+            else
+            {
+                // variable 'm' is not in scope here, and can't be used.
+                Console.WriteLine($"{a.GetType().Name} is not a Mammal");
+            }
+        }
+
+        static void TestForMammals(object o)
+        {
+            // You also can use the as operator and test for null
+            // before referencing the variable.
+            var m = o as Mammal;
+            if (m != null)
+            {
+                Console.WriteLine(m.ToString());
+            }
+            else
+            {
+                Console.WriteLine($"{o.GetType().Name} is not a Mammal");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SampleApp
+{
+    public class BankAccount
+    {
+        private static int accountNumberSeed = 1234567890;
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+
+        public BankAccount(string name)
+        {
+            Number = accountNumberSeed.ToString(); // "1234567890"
+            accountNumberSeed++;
+
+            Balance = 50m;
+            Owner = name;
+    }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+        }
+
+    }
+
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int balance = 0;
+    //        BankAccount account1 = new BankAccount("John");
+    //        BankAccount account2 = new BankAccount("Mary");
+
+
+    //        Console.WriteLine($"Account {account1.Number} was created for {account1.Owner} with {account1.Balance} initial balance.");
+    //        Console.WriteLine($"Account {account2.Number} was created for {account2.Owner} with {account2.Balance} initial balance.");
+
+    //    }
+    //}
+}
